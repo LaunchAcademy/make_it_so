b1c3233419c75790166dc47ed06b5bc99606fcc7
Cassandra Newell <cassandra.newell@protonmail.com>
2019-08-30 14:21:26 -0400

    expand tests for webpack(er) in package.json

--- spec/features/user_generates_rails_spec.rb
+++ spec/features/user_generates_rails_spec.rb
@@ -45,6 +45,11 @@ feature 'user generates rails app' do
 
   scenario 'installs webpack and webpacker' do
     in_package_json?(File.join(app_path, 'package.json')) do |json|
+      expect(json["dependencies"]["@rails/webpacker"]).to_not be_nil
+      expect(json["devDependencies"]["webpack"]).to_not be_nil
+      expect(json["devDependencies"]["webpack-cli"]).to_not be_nil
+      expect(json["devDependencies"]["webpack-dev-server"]).to_not be_nil
+
       expect(major_version(json["dependencies"]["@rails/webpacker"])).to be 4
       expect(major_version(json["devDependencies"]["webpack"])).to be 4
       expect(major_version(json["devDependencies"]["webpack-cli"])).to be 3
